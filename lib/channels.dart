75fd020cf2e9ff52152a3d369e7a14c7e328c73d
Simon Horlick <simonhorlick@gmail.com>
2017-11-19 14:01:02 +0800

    Add some more rpcs

--- lib/channels.dart
+++ lib/channels.dart
@@ -1,11 +1,14 @@
 import 'dart:async';
 
+import 'package:fixnum/fixnum.dart';
 import 'package:flutter/material.dart';
 
 import 'package:grpc/grpc.dart';
 
 import 'generated/rpc.pbgrpc.dart';
 import 'package:flutter/services.dart' show rootBundle;
+
+import 'package:convert/convert.dart';
 
 /// A ChannelWrapper calls the listChannels RPC on initialisation then renders
 /// nothing until the RPC returns.
@@ -18,6 +21,7 @@ class ChannelWrapper extends StatefulWidget {
 
 class _ChannelWrapperState extends State<ChannelWrapper> {
   List<ActiveChannel> channels;
+  Int64 balance;
 
   @override
   initState() {
@@ -26,7 +30,60 @@ class _ChannelWrapperState extends State<ChannelWrapper> {
         .listChannels(ListChannelsRequest.create())
         .then((response) => setState(() {
               channels = response.channels;
-            }));
+            }))
+        .catchError((error) => print("listChannels failed"));
+
+    // This doesn't work. Not sure why.
+    widget.stub.subscribeTransactions(GetTransactionsRequest.create())
+        .listen(onTransaction, onError: (error) => print("subscribeTransactions failed $error"));
+
+    refreshBalance();
+  }
+
+  void onTransaction(Transaction transaction) {
+    print(transaction);
+  }
+
+  void refreshBalance() {
+    widget.stub.walletBalance(WalletBalanceRequest.create()..witnessOnly = true).then((response) {
+      setState(() {
+        balance = response.balance;
+      });
+    })
+    .catchError((error) => print("walletBalance failed"));
+  }
+
+  void makeChannel() {
+    OpenChannelRequest request = OpenChannelRequest.create()
+      ..nodePubkey = hex.decoder.convert("03f3ae3c64338b0545ba12493421c6782aae7e43be7332f5658a5ae29cea0119da")
+      ..localFundingAmount = new Int64(500000);
+    widget.stub.openChannel(request).map(_processStatusUpdate);
+  }
+
+  _processStatusUpdate(OpenStatusUpdate event) {
+    print(event);
+  }
+
+  void connect() {
+    LightningAddress address = LightningAddress.create()
+      ..pubkey = "03f3ae3c64338b0545ba12493421c6782aae7e43be7332f5658a5ae29cea0119da"
+      ..host = "sg.horlick.me";
+    ConnectPeerRequest request = ConnectPeerRequest.create()
+      ..addr = address
+      ..perm = true;
+    widget.stub.connectPeer(request).then((response) {
+      print(response);
+    })
+    .catchError((error) => print("connectPeer failed"));
+  }
+
+  void createAddress() {
+    NewAddressRequest request = NewAddressRequest.create()
+    ..type = NewAddressRequest_AddressType.NESTED_PUBKEY_HASH;
+    widget.stub.newAddress(request).then((response) {
+      print(response);
+    })
+    .catchError((error) => print("newAddress failed"));
   }
 
   @override
@@ -34,7 +91,26 @@ class _ChannelWrapperState extends State<ChannelWrapper> {
     return channels == null
         ? new Center(child: new Text("Loading channels"))
         : channels.isEmpty
-            ? new Center(child: new Text("No channels"))
+            ? new Center(child:
+                new Column(
+                    children: <Widget>[
+                      new Text("No channels"),
+                      new Text("Balance is $balance"),
+                      new GestureDetector(
+                        onTap: connect,
+                        child: new Padding(padding: const EdgeInsets.all(20.0), child: new Text("Connect to peer")),
+                      ),
+                      new GestureDetector(
+                        onTap: makeChannel,
+                        child: new Padding(padding: const EdgeInsets.all(20.0), child: new Text("Make a channel")),
+                      ),
+                      new GestureDetector(
+                        onTap: createAddress,
+                        child: new Padding(padding: const EdgeInsets.all(20.0), child: new Text("Create a new address")),
+                      )
+                    ]
+                )
+            )
             : new Channels(channels);
   }
 }
@@ -53,7 +129,7 @@ class Channels extends StatelessWidget {
         dense: false,
         leading: new ExcludeSemantics(
             child: new CircleAvatar(child: new Text(channel.channelPoint))),
-        title: new Text('This item represents ${channel.channelPoint}.'),
+        title: new Text('${channel.channelPoint.substring(0,10)}...'),
         subtitle: new Text(
           'Capacity is ${channel.capacity}, ours is ${channel.localBalance} theirs is ${channel.remoteBalance}',
         ),
