2c8746925dd78ef9587947820b10cff33cede47c
Simon Horlick <simonhorlick@gmail.com>
2017-11-20 20:04:38 +0800

    Show peers in channels screen

--- lib/channels.dart
+++ lib/channels.dart
@@ -27,13 +27,17 @@ class _ChannelWrapperState extends State<ChannelWrapper> {
 
   Timer pollingTimer;
 
+  List<Peer> peers;
+
   @override
   initState() {
     super.initState();
     refreshChannels();
+    refreshPeers();
 
-    pollingTimer = new Timer.periodic(new Duration(seconds: 1), (timer) {
+    pollingTimer = new Timer.periodic(new Duration(seconds: 5), (timer) {
       refreshChannels();
+      refreshPeers();
     });
   }
 
@@ -64,7 +68,7 @@ class _ChannelWrapperState extends State<ChannelWrapper> {
   void makeChannel() {
     OpenChannelRequest request = OpenChannelRequest.create()
       ..nodePubkey = hex.decoder.convert(
-          "03f3ae3c64338b0545ba12493421c6782aae7e43be7332f5658a5ae29cea0119da")
+          "0294ceb8edf4b54da71caa506723dc8ab9c129ae19da4267f0e6d7cdcb396615b0")
       ..localFundingAmount = new Int64(500000);
     widget.stub.openChannel(request).map(_processStatusUpdate);
   }
@@ -76,50 +80,60 @@ class _ChannelWrapperState extends State<ChannelWrapper> {
   void connect() {
     LightningAddress address = LightningAddress.create()
       ..pubkey =
-          "03f3ae3c64338b0545ba12493421c6782aae7e43be7332f5658a5ae29cea0119da"
+          "0294ceb8edf4b54da71caa506723dc8ab9c129ae19da4267f0e6d7cdcb396615b0"
       ..host = "sg.horlick.me";
     ConnectPeerRequest request = ConnectPeerRequest.create()
       ..addr = address
-      ..perm = true;
+      ..perm = false;
     widget.stub.connectPeer(request).then((response) {
       print(response);
     }).catchError((error) => print("connectPeer failed: $error"));
   }
 
+  void refreshPeers() {
+    widget.stub.listPeers(ListPeersRequest.create()).then((response) {
+      setState(() {
+        peers = response.peers;
+      });
+    });
+  }
+
+  void disconnectAll() {
+    widget.stub.listPeers(ListPeersRequest.create()).then((response) {
+      response.peers.forEach((peer) {
+        widget.stub.disconnectPeer(DisconnectPeerRequest.create()..pubKey = peer.pubKey);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    var waitingForRpcs = channels == null || openingChannels == null;
+    var waitingForRpcs = channels == null || openingChannels == null || peers == null;
 
     return waitingForRpcs
         ? new Center(child: new Text("Loading channels"))
-        : (channels.isEmpty && openingChannels.isEmpty)
+        : (channels.isEmpty && openingChannels.isEmpty && peers.isEmpty)
             ? new Center(
                 child: new Column(children: <Widget>[
-                new Text("No channels"),
-                new GestureDetector(
-                  onTap: connect,
-                  child: new Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: new Text("Connect to peer")),
-                ),
-                new GestureDetector(
-                  onTap: makeChannel,
-                  child: new Padding(
-                      padding: const EdgeInsets.all(20.0),
-                      child: new Text("Make a channel")),
-                ),
+                new Text("No connections."),
               ]))
-            : new Channels(channels, openingChannels);
+            : new Channels(channels, openingChannels, peers, makeChannel, connect);
   }
 }
 
 /// Channels is the main UI component for rendering a list of payment channels.
 /// Note that this class is stateless to simplify its implementation.
 class Channels extends StatelessWidget {
-  Channels(this.channels, this.openingChannels);
+
+  Channels(this.channels, this.openingChannels, this.peers, this.makeChannel, this.connect);
 
   final List<ActiveChannel> channels;
   final List<PendingChannelResponse_PendingOpenChannel> openingChannels;
+  final List<Peer> peers;
+
+  // For debugging.
+  Function connect;
+  Function makeChannel;
 
   Widget buildListTile(BuildContext context, ActiveChannel channel) {
     return new MergeSemantics(
@@ -157,17 +171,58 @@ class Channels extends StatelessWidget {
     );
   }
 
+  Widget buildPeerTile(
+      BuildContext context, Peer peer) {
+    return new MergeSemantics(
+      child: new ListTile(
+        isThreeLine: true,
+        dense: false,
+        leading: new ExcludeSemantics(
+            child: new CircleAvatar(
+                child: new Text("${peer.peerId}"))),
+        title: new Text('Peer: ${peer.pubKey}',
+            overflow: TextOverflow.ellipsis),
+        subtitle: new Text(
+          'Sent ${peer.satSent}. Received ${peer.satRecv}',
+        ),
+        trailing: null,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    Iterable<Widget> listTiles =
+    Iterable<Widget> channelTiles =
         channels.map((channel) => buildListTile(context, channel));
     Iterable<Widget> openingListTiles = openingChannels
         .map((channel) => buildOpeningListTile(context, channel));
 
+    peers.sort((a,b) => a.peerId.compareTo(b.peerId));
+    Iterable<Widget> peerRows = peers.map((peer) => buildPeerTile(context, peer));
+
     return new Scrollbar(
       child: new ListView(
         padding: new EdgeInsets.symmetric(vertical: 8.0),
-        children: openingListTiles.toList()..addAll(listTiles),
+        children: [
+          new RaisedButton(
+            onPressed: connect,
+            child: new Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: new Text("Connect to peer")),
+          ),
+          new RaisedButton(
+            onPressed: makeChannel,
+            child: new Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: new Text("Make a channel")),
+          ),
+//          new RaisedButton(
+//            onPressed: disconnectAll,
+//            child: new Padding(
+//                padding: const EdgeInsets.all(20.0),
+//                child: new Text("Disconnect peers")),
+//          ),
+        ]..addAll(peerRows)..addAll(openingListTiles)..addAll(channelTiles),
       ),
     );
   }
