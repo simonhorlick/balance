8c8d97b52b74abdcabb9039515de7f87a5f85d91
Simon Horlick <simonhorlick@gmail.com>
2017-11-23 15:49:35 +0800

    go: Correctly serialise calls to Pause and Resume

--- client/client.go
+++ client/client.go
@@ -1,10 +1,22 @@
 package client
 
-import "github.com/simonhorlick/balance/lnd"
+import (
+	"github.com/simonhorlick/balance/lnd"
+	"sync"
+	"log"
+)
 
 func Start(dataDir string) error {
 	return lnd.Start(dataDir)
 }
+
+// The mutex guards the Pause and Resume functionality, ensuring that calls
+// to Pause and Resume are serialised and that duplicate calls are handled
+// correctly.
+var mutex = &sync.Mutex{}
+
+// Guarded by mutex.
+var running = false
 
 // On iOS the system may choose to reclaim resources while the app is
 // suspended. In this case, the socket grpc uses for listening can be reclaimed
@@ -12,10 +24,28 @@ func Start(dataDir string) error {
 // this case is to destroy the grpc server when the app is backgrounded and
 // re-create it again in Resume.
 func Pause() {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if !running {
+		log.Print("Lnd already paused")
+		return
+	}
+	running = false
+
 	lnd.Pause()
 }
 
 func Resume() {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if running {
+		log.Print("Lnd already running")
+		return
+	}
+	running = true
+
 	lnd.Resume()
 }
 
