8b7e428d2a6309228eac28259e15f6ec745af717
Simon Horlick <simonhorlick@gmail.com>
2018-05-23 10:32:12 +0800

    build: Specify location of protoc-gen-dart explicitly

--- generate_stubs.sh
+++ generate_stubs.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-# Assumes you have installed protoc_plugin globally, i.e.
+# Assumes you have installed protoc_plugin globally and that you have dart
+# installed, i.e.
+#  brew tap dart-lang/dart
+#  brew install dart --devel
 #  pub global activate protoc_plugin
 #  PATH=$PATH:$HOME/.pub-cache/bin
 
 mkdir -p lib/generated
 
-# Assumes you have dart installed, i.e.
-#  brew tap dart-lang/dart
-#  brew install dart
 protoc \
   -I. \
   -Ithird_party/googleapis \
+  --plugin=$HOME/.pub-cache/bin/protoc-gen-dart \
   --dart_out=grpc:lib/generated \
   vendor/github.com/lightningnetwork/lnd/lnrpc/rpc.proto
